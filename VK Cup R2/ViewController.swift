a5288e4ac57b1768252afd0522500f533baf2b4f
KurbatovIOS <akurbatov.work@gmail.com>
2023-01-05 13:26:56 +0300

    Initial Commit

---
+++ VK Cup R2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Cup R2
+//
+//  Created by Kurbatov Artem on 05.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
