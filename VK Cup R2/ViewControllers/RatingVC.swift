e61b57733c450cb7882e30de41cdf420e50a45ba
KurbatovIOS <akurbatov.work@gmail.com>
2023-01-11 12:48:16 +0300

    Finished rating view

--- VK Cup R2/ViewControllers/RatingVC.swift
+++ VK Cup R2/ViewControllers/RatingVC.swift
@@ -10,19 +10,81 @@ import UIKit
 class RatingVC: UIViewController {
     
     private let titleLabel = UILabel()
-    
-    private let star1ImageView = UIImageView()
-    private let star2ImageView = UIImageView()
-    private let star3ImageView = UIImageView()
-    private let star4ImageView = UIImageView()
-    private let star5ImageView = UIImageView()
+        
+    private var starButtonCollection = [UIButton]()
     
     private let starsStack = UIStackView()
+    
+    private var rating = 0 {
+        didSet {
+            for starButton in starButtonCollection {
+                let imageName = starButton.tag < rating ? "star.fill" : "star"
+                starButton.setImage(UIImage(systemName: imageName), for: .normal)
+                starButton.tintColor = starButton.tag < rating ? .systemOrange : .systemGray2
+                starButton.addTarget(self, action: #selector(updateRating), for: .touchUpInside)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .purple
+        view.backgroundColor = .systemBackground
+        configureStars()
+        rating = 0
+        configureTitle()
     }
     
+    private func configureTitle() {
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.text = "Пожалуйста, оцените статью"
+        titleLabel.textAlignment = .center
+        titleLabel.font = .preferredFont(forTextStyle: .title2)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: starsStack.topAnchor, constant: -40)
+        ])
+        
+    }
+    
+    private func configureStars() {
+        
+        view.addSubview(starsStack)
+        
+        starsStack.axis = .horizontal
+        starsStack.distribution = .equalSpacing
+        starsStack.spacing = 5
+        starsStack.alignment = .center
+        
+        for i in 0..<5 {
+            let starButton = UIButton()
+            starButton.tag = i
+            starButton.addTarget(self, action: #selector(updateRating), for: .touchUpInside)
+            starButtonCollection.append(starButton)
+            starsStack.addArrangedSubview(starButton)
+        }
+                        
+        starsStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            starsStack.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4),
+            starsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            starsStack.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
+        ])
+    }
+    
+    @objc private func updateRating(sender: UIButton) {
+        rating = sender.tag + 1
+        titleLabel.alpha = 0
+        titleLabel.text = "Спасибо за ваш отзыв!"
+        UIView.animate(withDuration: 1) {
+            self.titleLabel.alpha = 1
+        }
+    }
 }
