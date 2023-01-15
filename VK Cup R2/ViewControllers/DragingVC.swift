9953cf7ac4ee9b74f500b9b632aeb7fdf8a82385
KurbatovIOS <akurbatov.work@gmail.com>
2023-01-15 20:47:26 +0300

    drag view configuration made function to detect cell location and answer gaps locations

--- VK Cup R2/ViewControllers/DragingVC.swift
+++ VK Cup R2/ViewControllers/DragingVC.swift
@@ -13,12 +13,17 @@ class DragingVC: UIViewController {
     private let answersCollectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 200, left: 0, bottom: 0, right: 0)
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        layout.sectionInset = UIEdgeInsets(top: 300, left: 15, bottom: 0, right: 15)
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         return view
     }()
     
+    private var initialCenter = CGPoint()
+    
     private let mainModel = MainModel()
+    
+    private let answers = ["0", "1", "2", "3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +41,9 @@ class DragingVC: UIViewController {
         
         view.addSubview(questionLabel)
         
-        questionLabel.text = "А вот и текст"
+        questionLabel.text = "Сначала идёт _____, а потом уже потомпотомпо ______ !"
         questionLabel.font = .preferredFont(forTextStyle: .title2)
-        questionLabel.textAlignment = .center
+        questionLabel.textAlignment = .left
         questionLabel.numberOfLines = 0
         
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -61,21 +66,71 @@ class DragingVC: UIViewController {
         answersCollectionView.isScrollEnabled = false
         
         answersCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        
+    
         NSLayoutConstraint.activate([
-            answersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            answersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            //answersCollectionView.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 200),
-            answersCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
-            answersCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15)
+            answersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            answersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            answersCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            answersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    private func findCoordinates(of text: String) -> CGPoint {
+        let range: NSRange = (questionLabel.text! as NSString).range(of: text)
+        let prefix = (questionLabel.text! as NSString).substring(to: range.location)
+        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: questionLabel.font!])
+        var point = CGPoint(x: size.width + 15, y: 35)
+        if point.x > view.frame.width {
+            point.y += 30
+            point.x -= view.frame.width
+        }
+        
+        print("Text location \(point)")
+        
+        return point
+    }
+    
+    private func comparePositions(of cellPosition: CGPoint, and gapPoint: CGPoint) {
+        
+        if gapPoint.y - 10 <= cellPosition.y  && cellPosition.y <= gapPoint.y + 10 {
+            print("Ok")
+        }
+        
+    }
+    
+    @objc private func handlePenGesture(gesture: UIPanGestureRecognizer) {
+        
+        let cell = gesture.view!
+        let translation = gesture.translation(in: cell.superview)
+        
+        if gesture.state == .began {
+           
+            self.initialCenter = cell.center
+        }
+        else if gesture.state == .changed {
+            var newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            if newCenter.y <= 25 {
+                newCenter.y = 25
+            }
+            cell.center = newCenter
+        }
+        else if gesture.state == .ended {
+            let point = findCoordinates(of: "_____")
+            // adjust y range
+            print(point)
+            print(cell.center)
+            comparePositions(of: cell.center, and: point)
+        }
+        else {
+            cell.center = initialCenter
+        }
     }
 }
 
 extension DragingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return answers.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -86,7 +141,8 @@ extension DragingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICol
         let cell = answersCollectionView.dequeueReusableCell(withReuseIdentifier: Helpers.dragVCIdentifier, for: indexPath) as! DragCollectionViewCell
         cell.backgroundColor = .purple
         cell.tag = indexPath.row
-        cell.configureCell(text: "\(indexPath.row)")
+        cell.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector (handlePenGesture)))
+        cell.configureCell(text: answers[indexPath.row])
         return cell
     }
 }
--- VK Cup R2/Views/DragCollectionViewCell.swift
+++ VK Cup R2/Views/DragCollectionViewCell.swift
@@ -15,7 +15,7 @@ class DragCollectionViewCell: UICollectionViewCell {
     
     func configureCell(text: String) {
         
-        backgroundColor = .systemBackground
+        backgroundColor = .systemBlue.withAlphaComponent(0.3)
         
         addSubview(answerLabel)
 
@@ -29,38 +29,13 @@ class DragCollectionViewCell: UICollectionViewCell {
 
         answerLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePenGesture)))
         self.isUserInteractionEnabled = true
         
         NSLayoutConstraint.activate([
-            answerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
-            answerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            answerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            answerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
             answerLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             answerLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
         ])
-    }
-    
-    @objc private func handelTapGesture(gesture: UITapGestureRecognizer) {
-        
-        self.transform = CGAffineTransform(translationX: 180.3333282470703, y: 123.0)
-        
-    }
-    
-    @objc private func handlePenGesture(gesture: UIPanGestureRecognizer) {
-        
-        let cell = gesture.view!
-        let translation = gesture.translation(in: cell.superview)
-        
-        if gesture.state == .began {
-           
-            self.initialCenter = cell.center
-        }
-        else if gesture.state != .cancelled {
-            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
-            cell.center = newCenter
-        }
-        else {
-            cell.center = initialCenter
-        }
     }
 }
