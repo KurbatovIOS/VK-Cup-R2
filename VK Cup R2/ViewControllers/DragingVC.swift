b617b57d604d2e9d26708fdaacfec0a023682a78
KurbatovIOS <akurbatov.work@gmail.com>
2023-01-16 18:40:57 +0300

    answer checking answer highlights

--- VK Cup R2/ViewControllers/DragingVC.swift
+++ VK Cup R2/ViewControllers/DragingVC.swift
@@ -46,6 +46,8 @@ class DragingVC: UIViewController {
         displayQuestion()
     }
     
+    // MARK: - Views configuration
+    
     private func configureQuestion() {
         
         view.addSubview(questionLabel)
@@ -57,8 +59,8 @@ class DragingVC: UIViewController {
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             questionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
         ])
     }
@@ -74,7 +76,7 @@ class DragingVC: UIViewController {
         answersCollectionView.isScrollEnabled = false
         
         answersCollectionView.translatesAutoresizingMaskIntoConstraints = false
-    
+        
         NSLayoutConstraint.activate([
             answersCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             answersCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -103,95 +105,58 @@ class DragingVC: UIViewController {
         ])
     }
     
+    // MARK: - Display question
+    
     private func displayQuestion() {
         
         questionLabel.text = questions[currentQuestionIndex].question
+        answersCollectionView.reloadData()
     }
     
-    private func findCoordinates(of text: String) -> CGPoint {
-        let range: NSRange = (questionLabel.text! as NSString).range(of: text)
-        let prefix = (questionLabel.text! as NSString).substring(to: range.location)
-        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: questionLabel.font!])
-        var point = CGPoint(x: size.width + 15, y: 35)
-        if point.x > view.frame.width {
-            point.y += 30
-            point.x -= view.frame.width
-        }
-        
-        print("Text location \(point)")
-        
-        return point
-    }
-    
-    private func comparePositions(of cellPosition: CGPoint, and gapPoint: CGPoint) {
-        
-        if gapPoint.y - 10 <= cellPosition.y  && cellPosition.y <= gapPoint.y + 10 {
-            print("Ok")
-        }
-        
-        
-        
-    }
-    
-    private func checkAnswer(answer: String, answerIndex: Int) -> Bool {
-        
-        // chech and then replace
-        
-        if let stratIndex = questionLabel.text!.firstIndex(of: "_") {
-            
-            let endIndex = questionLabel.text!.index(stratIndex, offsetBy: 5)
-            
-            let range = stratIndex..<endIndex
-            
-            questionLabel.text?.replaceSubrange(range, with: answer)
-
-            //return answer == questions[currentQuestionIndex].correctAnswers[answerIndex]
-            return true
-        }
-        else {
-            return false
-        }
-    }
+    // MARK: - Button action and cell dragging
     
     @objc func buttonAction() {
         
+        answersCollectionView.isUserInteractionEnabled = false
+        
         let cells = answersCollectionView.visibleCells
-                
-        for cell in cells {
-            print(cell.center)
+        
+        let answer1 = findAnswerCell(cells: cells, answerIndex: 0)
+        let answer2 = findAnswerCell(cells: cells, answerIndex: 1)
+        
+        if answer1.cell != nil && answer2.cell != nil {
+            let answer1Color: UIColor = answer1.isCorrectAnseer! ? .green : .red
+            let answer2Color: UIColor = answer2.isCorrectAnseer! ? .green : .red
+            
+            let words = questionLabel.text?.split(separator: " ")
+            
+            var stringForRange = makeStringForRange(words: words!, answerIndex: questions[currentQuestionIndex].answerIndexes[0])
+            let answer1Range = (stringForRange as NSString).range(of: String(questions[currentQuestionIndex].answers[answer1.cell!.tag]))
+            
+            stringForRange = makeStringForRange(words: words!, answerIndex: questions[currentQuestionIndex].answerIndexes[1])
+            let answer2Range = (stringForRange as NSString).range(of: String(questions[currentQuestionIndex].answers[answer2.cell!.tag]))
+            
+            let mutableAttributedString = NSMutableAttributedString.init(string: questionLabel.text!)
+            mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: answer1Color, range: answer1Range)
+            mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: answer2Color, range: answer2Range)
+            
+            questionLabel.attributedText = mutableAttributedString
+            
+            mainModel.clickAnimation(view: questionLabel)
+            
+            currentQuestionIndex += 1
+            
+            if currentQuestionIndex == questions.count {
+                currentQuestionIndex = 0
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                self.displayQuestion()
+            }
         }
-        
-        //questionLabel.text?.replaceSubrange(<#T##subrange: Range<String.Index>##Range<String.Index>#>, with: <#T##Collection#>)
-        
-//        let answer1Color: UIColor = checkAnswer(answer: String(answer[0].lowercased()), answerIndex: 0) ? .green : .red
-//        let answer2Color: UIColor = checkAnswer(answer: String(answer[1].lowercased()), answerIndex: 1) ? .green : .red
-//
-//        let words = questionLabel.text?.split(separator: " ")
-//
-//        var stringForRange = makeStringForRange(words: words!, answerIndex: questions[currentQuestionIndex].answerIndexes[0])
-//        let answer1Range = (stringForRange as NSString).range(of: String(answer[0].lowercased()))
-//
-//        stringForRange = makeStringForRange(words: words!, answerIndex: questions[currentQuestionIndex].answerIndexes[1])
-//        let answer2Range = (stringForRange as NSString).range(of: String(answer[1].lowercased()))
-//
-//        let mutableAttributedString = NSMutableAttributedString.init(string: questionLabel.text!)
-//        mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: answer1Color, range: answer1Range)
-//        mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: answer2Color, range: answer2Range)
-//
-//        questionLabel.attributedText = mutableAttributedString
-//
-//        mainModel.clickAnimation(view: questionLabel)
-//
-//        currentQuestionIndex += 1
-//
-//        if currentQuestionIndex == questions.count {
-//            currentQuestionIndex = 0
-//        }
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
-//            self.displayQuestion()
-//        }
-     
+        else {
+            // create alert
+        }
     }
     
     @objc private func handlePenGesture(gesture: UIPanGestureRecognizer) {
@@ -200,29 +165,99 @@ class DragingVC: UIViewController {
         let translation = gesture.translation(in: cell.superview)
         
         if gesture.state == .began {
-           
             self.initialCenter = cell.center
         }
-        else if gesture.state == .changed {
+        else if gesture.state != .cancelled {
+            
             var newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
             if newCenter.y <= 25 {
                 newCenter.y = 25
             }
             cell.center = newCenter
         }
-        else if gesture.state == .ended {
-            let point = findCoordinates(of: "_____")
-            
-            comparePositions(of: cell.center, and: point)
-        }
         else {
             cell.center = initialCenter
         }
     }
+    
+    // MARK: - Helper functions
+    
+    private func findPoint() -> CGPoint {
+        
+        let range: NSRange = (questionLabel.text! as NSString).range(of: "_____")
+        let prefix = (questionLabel.text! as NSString).substring(to: range.location)
+        let size: CGSize = prefix.size(withAttributes: [NSAttributedString.Key.font: questionLabel.font!])
+        var point = CGPoint(x: size.width + 15, y: 35)
+        if point.x > view.frame.width {
+            point.y += 30
+            point.x -= view.frame.width
+        }
+        
+        return point
+    }
+    
+    private func comparePositions(of cellPosition: CGPoint, and gapPoint: CGPoint) -> Bool {
+        
+        return (gapPoint.y - 10 <= cellPosition.y  && cellPosition.y <= gapPoint.y + 10) && (gapPoint.x < cellPosition.x && cellPosition.x < gapPoint.x + 60)
+    }
+    
+    private func checkAnswer(cellTag: Int, answerIndex: Int) -> Bool {
+        
+        if let stratIndex = questionLabel.text!.firstIndex(of: "_") {
+            
+            let endIndex = questionLabel.text!.index(stratIndex, offsetBy: 5)
+            
+            let range = stratIndex..<endIndex
+            
+            let answer = questions[currentQuestionIndex].answers[cellTag]
+            
+            questionLabel.text?.replaceSubrange(range, with: answer)
+            
+            return cellTag == questions[currentQuestionIndex].correctAnswers[answerIndex]
+        }
+        else {
+            return false
+        }
+    }
+    
+    private func findAnswerCell(cells: [UICollectionViewCell], answerIndex: Int) -> (cell: UICollectionViewCell?, isCorrectAnseer: Bool?) {
+        
+        let point = findPoint()
+        
+        for cell in cells {
+            if comparePositions(of: cell.center, and: point) {
+                cell.alpha = 0
+                return (cell, checkAnswer(cellTag: cell.tag, answerIndex: answerIndex))
+            }
+        }
+        
+        return (nil, nil)
+    }
+    
+    private func makeStringForRange(words: [String.SubSequence], answerIndex: Int) -> String {
+        
+        var str = ""
+        
+        for i in 0..<words.count {
+            
+            if i != answerIndex {
+                str += String(repeating: "#", count: words[i].count)
+            }
+            else {
+                str += words[i]
+            }
+            
+            str += " "
+        }
+        
+        return String(str.dropLast(1))
+    }
 }
 
+// MARK: - Extensions
+
 extension DragingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-   
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return questions[currentQuestionIndex].answers.count
     }
@@ -233,7 +268,6 @@ extension DragingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICol
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = answersCollectionView.dequeueReusableCell(withReuseIdentifier: Helpers.dragVCIdentifier, for: indexPath) as! DragCollectionViewCell
-        cell.backgroundColor = .purple
         cell.tag = indexPath.row
         cell.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector (handlePenGesture)))
         cell.configureCell(text: questions[currentQuestionIndex].answers[indexPath.row])
@@ -242,7 +276,7 @@ extension DragingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICol
 }
 
 extension DragingVC: DragModelDelegate {
-   
+    
     func getData(_ data: [DragQuestion]) {
         self.questions = data
     }
